9579fcad4d0805ff1995ddc5d3faf9e210298f88
quoctinnguyen8 <nguyenquoctin9x@gmail.com>
2025-07-10 02:31:12 +0700

    deploy

--- jp-quiz/src/configs/apiConfig.js
+++
@@ -1,7 +0,0 @@
-const GET_DAILY_QUIZ =
-  "https://script.google.com/macros/s/AKfycbxw8dc4GTgNCABoOzuMnGS3Vo6sGIToc4vTy85CBKCYLzjsM9TQwkpo2FdYU54EuCey/exec";
-
-export default {
-  GET_DAILY_QUIZ,
-  // Add more API endpoints as needed
-};
